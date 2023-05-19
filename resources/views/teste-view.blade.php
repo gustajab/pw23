7e4d7229c8d39f6b7c196669605c2008e787d937
Gustavo Rajab <122064946+gustajab@users.noreply.github.com>
2023-05-19 11:14:40 -0300

    Initial commit

---
+++ resources/views/teste-view.blade.php
@@ -0,0 +1,9 @@
+@extends('includes.base')
+
+@section('title', 'titulo joia')
+
+@section('content')
+<p>Este é meu primeiro arquivo blade</p>
+<p>Estou emocionado😭</p>
+<p>Minha primeira variavel que veio de longe: {{$valor_da_controller}}</p>
+@endsection
