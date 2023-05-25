8bd9a88e31d10ee6c99b1d1dd1440794bfc8dd71
Gustavo Rajab <122064946+gustajab@users.noreply.github.com>
2023-05-25 10:09:53 -0300

    produtos

--- resources/views/teste-view.blade.php
+++ resources/views/teste-view.blade.php
@@ -5,5 +5,5 @@
 @section('content')
 <p>Este é meu primeiro arquivo blade</p>
 <p>Estou emocionado😭</p>
-<p>Minha primeira variavel que veio de longe: {{$valor_da_controller}}</p>
+<p>Minha primeira variavel que veio de longeee: {{$valor_da_controller}}</p>
 @endsection
