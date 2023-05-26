838a67fc9fbd6e2073f32cf3f109c103ef68a5b1
Gustavo Rajab <122064946+gustajab@users.noreply.github.com>
2023-05-26 11:11:41 -0300

    osso

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -1,3 +1,10 @@
 @extends('includes.base')
 
+@section('title', 'Produtos')
+
+@section('content')
+    <p>Produtos funcionando</p>
+    <a href="{{ route('produtos.add') }}">Adicionar produto</a>
+@endsection
+
 
